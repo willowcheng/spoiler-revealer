8dfc1f1346030cc5f9072b3b328b050e9bb91747
willowcheng <e.wingus@Gmail.com>
2015-08-06 00:16:15 -0400

    Perfect

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ $(".spoiler span").hide();
 $(".spoiler").append("<button>Reveal Spoiler!</button>");
 // 3, When button pressed
 $("button").click(function () {
-    // 3.1, Show spoiler
-    $(".spoiler span").show();
+    // 3.1, Show spoiler next to the button clicked
+    $(this).prev().show();
     // 3.2, Get rid of button
     $(this).remove();
 });
